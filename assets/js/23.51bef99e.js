882349dd399b7ff7fc76c44e3902905824557a5a
Hervé <herve@cocoom.com>
2020-05-02 11:48:00 +0200

    Update with sidebar + custom colors + french homepage

---
+++ assets/js/23.51bef99e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{350:function(t,e,n){"use strict";n.r(e);var o=n(18),s=Object(o.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("p",[this._v("About setting up your Cocoom")])])}),[],!1,null,null,null);e.default=s.exports}}]);
