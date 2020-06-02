710349432b6cb34d92215a5f821bd01e826f2696
Hervé <herve@cocoom.com>
2020-06-02 10:37:36 +0200

    New: (CSM) Add Intercom badge to help center

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -69,6 +69,17 @@ module.exports = {
     sidebarDepth: 2
   },
 
+  head: [
+    ['script', {}, `
+      window.intercomSettings = {
+        app_id: "a52qmjhg"
+      };
+    `],
+    ['script', {}, `
+      (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/a52qmjhg';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+    `],
+  ],
+
   plugins: [
     'check-md',
     '@vuepress/medium-zoom',
@@ -88,7 +99,7 @@ module.exports = {
     [
       '@vuepress/google-analytics',
       {
-        'ga': 'UA-130889610-1' // UA-00000000-0
+        'ga': 'UA-130889610-1'
       }
     ]
   ]
