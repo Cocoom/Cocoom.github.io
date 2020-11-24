94338ed86846da3bff7f7c070cfa53e16895f38a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-24 08:18:22 +0000

    Deploy Cocoom/cocoom.github.io to github.com/Cocoom/cocoom.github.io.git:master

--- assets/js/4.9a6633f3.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{358:function(e,t,n){"use strict";n.r(t);n(44),n(67);var o={name:"Redirect",props:{to:{type:String,required:!0}},beforeMount:function(){document.location.replace(this.to)}},r=n(25),i=Object(r.a)(o,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);t.default=i.exports}}]);
