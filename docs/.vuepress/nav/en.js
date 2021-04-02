12265dbee053be5cc35b4286a4631a8f4acf53f4
Hervé <herve@cocoom.com>
2021-04-02 16:26:53 +0200

    New: (external links) Add navigation link to our public roadmap

--- docs/.vuepress/nav/en.js
+++ docs/.vuepress/nav/en.js
@@ -1,3 +1,4 @@
 module.exports = [
-  { text: 'cocoom.com', link: 'https://cocoom.com?lang=en', target:'_blank', rel:'' }
+  { text: 'cocoom.com', link: 'https://cocoom.com?lang=en', target: '_blank', rel: '' },
+  { text: 'Roadmap', link: 'https://trello.com/b/UdSncTt9', target: '_blank', rel: '' }
 ];
